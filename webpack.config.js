8a47ea97520b9f2b1618ecf0cd31908140edbae7
David  Francisco <davidfrancisco@Davids-MacBook-Air.local>
2022-10-05 17:15:46 -0700

    Pathing changes.

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.css'],
-    modules: [path.resolve(__dirname, 'src')],
+    modules: [path.resolve(__dirname, 'dist')],
   },
   module: {
     rules: [
